ee575240883343558646fe8d42ebfa44450024d5
Dru61 <72609604+Dru61@users.noreply.github.com>
2020-12-03 10:44:11 -0400

    Update search.js

--- src/util/music/search.js
+++ src/util/music/search.js
@@ -6,16 +6,7 @@ module.exports = async function search(s, message) {
     if (!message.member.voice.channel) {
         return message.reply('você precisa estar conectado em algum canal de voz!')
     };
-    if (s.match(/https:\/\/www.youtube.com\/playlist\?list=/g)) {
-        videos = await youtube.getPlaylist(s).then(p => p.getVideos().then(v => v))     
-        result = {
-            url: 'https://www.youtube.com/watch?v='+videos.map(v => v.id),
-            title: null,
-            id: null,
-        }
-        duration = null
-    }
-    else if (s.match(/https:\/\/www.youtube.com\/watch\?v=/g)) {
+    if (s.match(/https:\/\/www.youtube.com\/watch\?v=/g)) {
         result = await youtube.getVideo(s).then(r => r)
         duration = (await ytsearch( { videoId: result.id })).seconds
     }
