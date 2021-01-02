8f45cc5648b793abdd5160d1ad6ef9d894b4607e
Dru61 <72609604+Dru61@users.noreply.github.com>
2021-01-02 09:20:58 -0400

    Update eval.js

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -10,22 +10,14 @@ module.exports = {
         if (code.includes('--hide')) {
             hide = true
             code = code.replace('--hide', '')
-        }
+        };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         mentioned = message.mentions.users
             if (mentioned && code.includes('--user')) {
             code = code.replace('--user', '').replace(/<@!?(\d{16,18})>/g, `yukie.users.fetch(message.mentions.users.map(u => u.id))`)
             hide = false
-        }
+        };
         delete mentioned
-=======
-            if (message.mentions.users.first()) code = code.replace(`<@!${message.mentions.users.first().id}>`, `yukie.users.fetch(message.mentions.users.first().id)`)
->>>>>>> c3290346518f4a1827bce1af71f1a97df69c96f2
-=======
-            if (message.mentions.users.first() && code.includes("--user")) code = code.replace('--user', '').replace(`<@!${message.mentions.users.first().id}>`, `yukie.users.fetch(message.mentions.users.first().id)`)
->>>>>>> 7a89c5e17e21e35e577c060f21839984904dd286
 
         if (code === 'ncu --ver') {
             message.channel.send('```js\n\'checking for updates...\'```')
